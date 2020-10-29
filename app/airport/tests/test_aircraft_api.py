be36f1b91e62b829a0a66d39793387aea8b9ec5d
Milorad Kukic <kukic.milorad@gmail.com>
2020-10-29 21:05:10 +0000

    Refactored tests.

--- app/airport/tests/test_aircraft_api.py
+++ app/airport/tests/test_aircraft_api.py
@@ -8,7 +8,7 @@ from rest_framework import status
 from airport.models import Aircraft
 
 
-class AircraftPrivateApiTests(TestCase):
+class AircraftPrivateApiInvalidKeyTests(TestCase):
 
     def setUp(self):
         self.client = APIClient()
@@ -37,55 +37,59 @@ class AircraftPrivateApiTests(TestCase):
         saved_aircraft = Aircraft.objects.first()
         self.assertIsNone(saved_aircraft)
 
-    @patch('airport.views.public_key_is_valid')
-    def test_should_save_new_aircraft_if_valid_request_is_sent(self, public_key_is_valid):
+
+class AircraftPrivateApiTests(TestCase):
+
+    def setUp(self):
+        self.client = APIClient()
+        self.patcher = patch('airport.views.public_key_is_valid')
+        self.public_key_is_valid = self.patcher.start()
+        self.public_key_is_valid.return_value = True
+
+    def tearDown(self):
+        self.patcher.stop()
+
+    def test_should_save_new_aircraft_if_valid_request_is_sent(self):
         SENT_CALL_SIGN = 'NC9574'
         payload = {
             'state': 'TAKE_OFF',
             'public_key': 'dummy_public_key_that_we_consider_valid'
         }
-        public_key_is_valid.return_value = True
 
         self.client.post(f'/api/{SENT_CALL_SIGN}/intent/', payload)
 
         saved_aircraft = Aircraft.objects.get(call_sign=SENT_CALL_SIGN)
         self.assertEqual(saved_aircraft.call_sign, SENT_CALL_SIGN)
 
-    @patch('airport.views.public_key_is_valid')
-    def test_valid_take_off_request_should_return_204_no_content(self, public_key_is_valid):
+    def test_valid_take_off_request_should_return_204_no_content(self):
         SENT_CALL_SIGN = 'AB1234'
         payload = {
             'state': 'TAKE_OFF',
             'public_key': 'dummy_public_key_that_we_consider_valid'
         }
-        public_key_is_valid.return_value = True
 
         res = self.client.post(f'/api/{SENT_CALL_SIGN}/intent/', payload)
 
         self.assertEqual(res.status_code, status.HTTP_204_NO_CONTENT)
         self.assertIsNone(res.data)
 
-    @patch('airport.views.public_key_is_valid')
-    def test_known_aircraft_sends_valid_request_return_204_no_content(self, public_key_is_valid):
+    def test_known_aircraft_sends_valid_request_return_204_no_content(self):
         aircraft = Aircraft.objects.create(call_sign='ABCD')
         payload = {
             'state': 'TAKE_OFF',
             'public_key': 'valid public key'
         }
-        public_key_is_valid.return_value = True
 
         res = self.client.post(f'/api/{aircraft.call_sign}/intent/', payload)
 
         self.assertEqual(res.status_code, status.HTTP_204_NO_CONTENT)
         self.assertIsNone(res.data)
 
-    @patch('airport.views.public_key_is_valid')
-    def test_state_not_passed_return_400_bad_request(self, public_key_is_valid):
+    def test_state_not_passed_return_400_bad_request(self):
         aircraft = Aircraft.objects.create(call_sign='ABCD')
         payload = {
             'public_key': 'valid public key'
         }
-        public_key_is_valid.return_value = True
 
         res = self.client.post(f'/api/{aircraft.call_sign}/intent/', payload)
 
@@ -96,15 +100,12 @@ class AircraftPrivateApiTests(TestCase):
     # TESTS FOR VALID WORKFLOW SWITCHING #
     ######################################
 
-    @patch('airport.views.public_key_is_valid')
-    def test_aircraft_can_switch_state_from_PARKED_to_TAKE_OFF(self, public_key_is_valid):
+    def test_aircraft_can_switch_state_from_PARKED_to_TAKE_OFF(self):
         aircraft = Aircraft.objects.create(call_sign='AB1234', state='PARKED')
         payload = {
             'state': 'TAKE_OFF',
             'public_key': 'valid public key'
         }
-        public_key_is_valid.return_value = True
-
         res = self.client.post(f'/api/{aircraft.call_sign}/intent/', payload)
 
         aircraft.refresh_from_db()
@@ -112,14 +113,12 @@ class AircraftPrivateApiTests(TestCase):
         self.assertEqual(res.status_code, status.HTTP_204_NO_CONTENT)
         self.assertEqual(aircraft.state, 'TAKE_OFF')
 
-    @patch('airport.views.public_key_is_valid')
-    def test_aircraft_can_switch_state_from_TAKE_OFF_to_AIRBORNE(self, public_key_is_valid):
+    def test_aircraft_can_switch_state_from_TAKE_OFF_to_AIRBORNE(self):
         aircraft = Aircraft.objects.create(call_sign='AB1234', state='TAKE_OFF')
         payload = {
             'state': 'AIRBORNE',
             'public_key': 'valid public key'
         }
-        public_key_is_valid.return_value = True
 
         res = self.client.post(f'/api/{aircraft.call_sign}/intent/', payload)
 
@@ -128,14 +127,12 @@ class AircraftPrivateApiTests(TestCase):
         self.assertEqual(res.status_code, status.HTTP_204_NO_CONTENT)
         self.assertEqual(aircraft.state, 'AIRBORNE')
 
-    @patch('airport.views.public_key_is_valid')
-    def test_aircraft_can_switch_state_from_AIRBORNE_to_APPROACH(self, public_key_is_valid):
+    def test_aircraft_can_switch_state_from_AIRBORNE_to_APPROACH(self):
         aircraft = Aircraft.objects.create(call_sign='AB1234', state='AIRBORNE')
         payload = {
             'state': 'APPROACH',
             'public_key': 'valid public key'
         }
-        public_key_is_valid.return_value = True
 
         res = self.client.post(f'/api/{aircraft.call_sign}/intent/', payload)
 
@@ -144,14 +141,12 @@ class AircraftPrivateApiTests(TestCase):
         self.assertEqual(res.status_code, status.HTTP_204_NO_CONTENT)
         self.assertEqual(aircraft.state, 'APPROACH')
 
-    @patch('airport.views.public_key_is_valid')
-    def test_aircraft_can_switch_state_from_APPROACH_to_LANDED(self, public_key_is_valid):
+    def test_aircraft_can_switch_state_from_APPROACH_to_LANDED(self):
         aircraft = Aircraft.objects.create(call_sign='AB1234', state='APPROACH')
         payload = {
             'state': 'LANDED',
             'public_key': 'valid public key'
         }
-        public_key_is_valid.return_value = True
 
         res = self.client.post(f'/api/{aircraft.call_sign}/intent/', payload)
 
@@ -160,14 +155,12 @@ class AircraftPrivateApiTests(TestCase):
         self.assertEqual(res.status_code, status.HTTP_204_NO_CONTENT)
         self.assertEqual(aircraft.state, 'LANDED')
 
-    @patch('airport.views.public_key_is_valid')
-    def test_aircraft_can_switch_state_from_APPROACH_to_AIRBORNE(self, public_key_is_valid):
+    def test_aircraft_can_switch_state_from_APPROACH_to_AIRBORNE(self):
         aircraft = Aircraft.objects.create(call_sign='AB1234', state='APPROACH')
         payload = {
             'state': 'AIRBORNE',
             'public_key': 'valid public key'
         }
-        public_key_is_valid.return_value = True
 
         res = self.client.post(f'/api/{aircraft.call_sign}/intent/', payload)
 
@@ -180,14 +173,12 @@ class AircraftPrivateApiTests(TestCase):
     # TESTS FOR INVALID WORKFLOW SWITCHING #
     ########################################
 
-    @patch('airport.views.public_key_is_valid')
-    def test_aircraft_cant_switch_state_from_PARKED_to_invalid_state(self, public_key_is_valid):
+    def test_aircraft_cant_switch_state_from_PARKED_to_invalid_state(self):
         aircraft = Aircraft.objects.create(call_sign='AB1234', state='PARKED')
         payload = {
             'state': 'INVALID STATE',
             'public_key': 'valid public key'
         }
-        public_key_is_valid.return_value = True
 
         res = self.client.post(f'/api/{aircraft.call_sign}/intent/', payload)
 
@@ -196,14 +187,12 @@ class AircraftPrivateApiTests(TestCase):
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertEqual(aircraft.state, 'PARKED')
 
-    @patch('airport.views.public_key_is_valid')
-    def test_PARKED_aircraft_cant_swith_to_other_state_than_TAKE_OFF(self, public_key_is_valid):
+    def test_PARKED_aircraft_cant_swith_to_other_state_than_TAKE_OFF(self):
         aircraft = Aircraft.objects.create(call_sign='AB1234', state='PARKED')
         payload = {
             'state': 'APPROACH',
             'public_key': 'valid public key'
         }
-        public_key_is_valid.return_value = True
 
         res = self.client.post(f'/api/{aircraft.call_sign}/intent/', payload)
 
@@ -212,14 +201,12 @@ class AircraftPrivateApiTests(TestCase):
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertEqual(aircraft.state, 'PARKED')
 
-    @patch('airport.views.public_key_is_valid')
-    def test_TAKE_OFF_aircraft_cant_swith_to_other_state_than_AIRBORNE(self, public_key_is_valid):
+    def test_TAKE_OFF_aircraft_cant_swith_to_other_state_than_AIRBORNE(self):
         aircraft = Aircraft.objects.create(call_sign='AB1234', state='TAKE_OFF')
         payload = {
             'state': 'APPROACH',
             'public_key': 'valid public key'
         }
-        public_key_is_valid.return_value = True
 
         res = self.client.post(f'/api/{aircraft.call_sign}/intent/', payload)
 
@@ -228,14 +215,12 @@ class AircraftPrivateApiTests(TestCase):
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertEqual(aircraft.state, 'TAKE_OFF')
 
-    @patch('airport.views.public_key_is_valid')
-    def test_AIRBORNE_aircraft_cant_swith_to_other_state_than_APPROACH(self, public_key_is_valid):
+    def test_AIRBORNE_aircraft_cant_swith_to_other_state_than_APPROACH(self):
         aircraft = Aircraft.objects.create(call_sign='AB1234', state='AIRBORNE')
         payload = {
             'state': 'TAKE_OFF',
             'public_key': 'valid public key'
         }
-        public_key_is_valid.return_value = True
 
         res = self.client.post(f'/api/{aircraft.call_sign}/intent/', payload)
 
@@ -244,14 +229,12 @@ class AircraftPrivateApiTests(TestCase):
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertEqual(aircraft.state, 'AIRBORNE')
 
-    @patch('airport.views.public_key_is_valid')
-    def test_APPROACH_aircraft_cant_swith_to_other_state_than_AIRBORNE_or_LANDED(self, public_key_is_valid):
+    def test_APPROACH_aircraft_cant_swith_to_other_state_than_AIRBORNE_or_LANDED(self):
         aircraft = Aircraft.objects.create(call_sign='AB1234', state='AIRBORNE')
         payload = {
             'state': 'TAKE_OFF',
             'public_key': 'valid public key'
         }
-        public_key_is_valid.return_value = True
 
         res = self.client.post(f'/api/{aircraft.call_sign}/intent/', payload)
 
