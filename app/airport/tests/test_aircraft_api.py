7cf982b3ed983ce04128512a2b55f8736c210822
Milorad Kukic <kukic.milorad@gmail.com>
2020-10-29 17:13:06 +0000

    Added validation of public key on intent api call.

--- app/airport/tests/test_aircraft_api.py
+++ app/airport/tests/test_aircraft_api.py
@@ -1,3 +1,5 @@
+from unittest.mock import patch
+
 from django.test import TestCase
 
 from rest_framework.test import APIClient
@@ -6,30 +8,73 @@ from rest_framework import status
 from airport.models import Aircraft
 
 
-class AircraftPrivateAPI(TestCase):
+class AircraftPrivateApiTests(TestCase):
 
     def setUp(self):
         self.client = APIClient()
 
-    def test_should_save_new_aircraft_if_valid_request_is_sent(self):
+    def test_public_key_not_sent_should_return_401_unauthorized(self):
+        SENT_CALL_SIGN = 'AB1234'
+        payload = {
+            'state': 'APPROACH'
+        }
+
+        res = self.client.post(f'/api/{SENT_CALL_SIGN}/intent/', payload)
+
+        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    @patch('airport.views.public_key_is_valid')
+    def test_invalid_public_key_should_return_401_unauthorized(self, public_key_is_valid):
+        SENT_CALL_SIGN = 'AB1234'
+        payload = {
+            'state': 'TAKE_OFF',
+            'public_key': 'invalid public key'
+        }
+        public_key_is_valid.return_value = False
+
+        self.client.post(f'/api/{SENT_CALL_SIGN}/intent/', payload)
+
+        saved_aircraft = Aircraft.objects.first()
+        self.assertIsNone(saved_aircraft)
+
+    @patch('airport.views.public_key_is_valid')
+    def test_should_save_new_aircraft_if_valid_request_is_sent(self, public_key_is_valid):
         SENT_CALL_SIGN = 'NC9574'
         payload = {
             'state': 'TAKE_OFF',
             'public_key': 'dummy_public_key_that_we_consider_valid'
         }
+        public_key_is_valid.return_value = True
 
         self.client.post(f'/api/{SENT_CALL_SIGN}/intent/', payload)
 
         saved_aircraft = Aircraft.objects.get(call_sign=SENT_CALL_SIGN)
         self.assertEqual(saved_aircraft.call_sign, SENT_CALL_SIGN)
 
-    def test_valid_take_off_request_should_return_204_no_content(self):
+    @patch('airport.views.public_key_is_valid')
+    def test_valid_take_off_request_should_return_204_no_content(self, public_key_is_valid):
         SENT_CALL_SIGN = 'AB1234'
         payload = {
             'state': 'TAKE_OFF',
             'public_key': 'dummy_public_key_that_we_consider_valid'
         }
+        public_key_is_valid.return_value = True
 
         res = self.client.post(f'/api/{SENT_CALL_SIGN}/intent/', payload)
 
         self.assertEqual(res.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertIsNone(res.data)
+
+    @patch('airport.views.public_key_is_valid')
+    def test_known_aircraft_sends_valid_request_return_204_no_content(self, public_key_is_valid):
+        aircraft = Aircraft.objects.create(call_sign='ABCD')
+        payload = {
+            'state': 'TAKE_OFF',
+            'public_key': 'valid public key'
+        }
+        public_key_is_valid.return_value = True
+
+        res = self.client.post(f'/api/{aircraft.call_sign}/intent/', payload)
+
+        self.assertEqual(res.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertIsNone(res.data)
--- app/airport/views.py
+++ app/airport/views.py
@@ -2,6 +2,8 @@ from rest_framework import viewsets, status
 from rest_framework.decorators import action
 from rest_framework.response import Response
 
+from Crypto.PublicKey import RSA
+
 from airport.models import Aircraft
 from airport.serializers import AircraftSerializer
 
@@ -14,5 +16,34 @@ class AircraftViewSet(viewsets.GenericViewSet):
             url_path='(?P<call_sign>[^/.]+)/intent',
             url_name='intent')
     def intent(self, request, call_sign=None, pk=None):
-        Aircraft.objects.create(call_sign=call_sign)
+        public_key = request.data.get('public_key', None)
+
+        if not public_key or not public_key_is_valid(public_key):
+            return Response(None, status=status.HTTP_401_UNAUTHORIZED)
+
+        if not request.data.get('public_key', None):
+            return Response(None, status=status.HTTP_401_UNAUTHORIZED)
+
+        try:
+            Aircraft.objects.get(call_sign=call_sign)
+        except Aircraft.DoesNotExist:
+            Aircraft.objects.create(call_sign=call_sign)
+
         return Response(None, status=status.HTTP_204_NO_CONTENT)
+
+
+def public_key_is_valid(public_key_content):
+    """ This method will answer if given public key matches
+    a given private key or not"""
+
+    private_key = None
+
+    with open("/keys/airport_ops_rsa") as private_key_file:
+        private_key = RSA.importKey(private_key_file.read())
+
+    if not private_key:
+        return False
+
+    public_key = RSA.importKey(public_key_content)
+
+    return private_key.publickey() == public_key
